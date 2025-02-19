6bc2d2760cd16a5ecf5edb998f2d1520334caee3
OtavioParisDeV <otavioparis.dev@gmail.com>
2025-02-18 22:52:57 -0300

    commit final

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -2,8 +2,6 @@ import java.sql.SQLOutput;
 import java.util.Locale;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class ContaTerminal {
 
     public static void main(String[] args) {
