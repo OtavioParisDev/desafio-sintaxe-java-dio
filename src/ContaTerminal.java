b0235c58cb632575ea92ebb343dd19e631015a70
OtavioParisDeV <otavioparis.dev@gmail.com>
2025-02-18 22:42:57 -0300

    teste 1

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,9 +1,35 @@
+import java.sql.SQLOutput;
+import java.util.Locale;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class ContaTerminal {
+
+
+
+
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        final double saldoNovoCliente = 1590.35;
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o seu nome ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o seu usuário ");
+        int numeroUsuario = scanner.nextInt();
+
+        System.out.println("Por favor, digite o numero de Agência ");
+        String agencia = scanner.next();
+
+
+        System.out.println("Olá " + nomeCliente +
+                ", obrigado por criar uma conta em nosso banco,"
+                + " sua agência é " + agencia + ", conta "
+                + numeroUsuario + "e seus saldo "
+                + saldoNovoCliente + "já está disponível para saque.");
 
 
         }
