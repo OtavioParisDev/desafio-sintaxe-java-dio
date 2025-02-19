6801bb35f8b215c68b2ff289fe8984dd3f7fda26
OtavioParisDeV <otavioparis.dev@gmail.com>
2025-02-18 22:51:35 -0300

    commit final

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -13,10 +13,11 @@ public class ContaTerminal {
 
         final double saldoNovoCliente = 1590.35;
 
+
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Por favor, digite o seu nome ");
-        String nomeCliente = scanner.next();
+        String nomeCliente = scanner.nextLine();
 
         System.out.println("Por favor, digite o seu usuário ");
         int numeroUsuario = scanner.nextInt();
@@ -28,8 +29,8 @@ public class ContaTerminal {
         System.out.println("Olá " + nomeCliente +
                 ", obrigado por criar uma conta em nosso banco,"
                 + " sua agência é " + agencia + ", conta "
-                + numeroUsuario + "e seus saldo "
-                + saldoNovoCliente + "já está disponível para saque.");
+                + numeroUsuario + " e seus saldo "
+                + saldoNovoCliente + " já está disponível para saque.");
 
 
         }
